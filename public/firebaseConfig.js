9be5cc5d349b6f8bbb01a56cec0d8fb49df05526
trantthien2503 <49374309+trantthien2503@users.noreply.github.com>
2024-06-24 16:22:00 +0700

    init project

---
+++ public/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAt6xsijlh_e3mo2mVioKEUsKttLn6IAzs",
+  authDomain: "manager-bill.firebaseapp.com",
+  projectId: "manager-bill",
+  storageBucket: "manager-bill.appspot.com",
+  messagingSenderId: "988844774598",
+  appId: "1:988844774598:web:fbfd2dbb827de7a01df3c7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ public/scripts.js
@@ -0,0 +1,21 @@
+const ctx = document.getElementById('billChart').getContext('2d');
+const billChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'Bills',
+      data: [12, 19, 3, 5, 2, 3, 7],
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
